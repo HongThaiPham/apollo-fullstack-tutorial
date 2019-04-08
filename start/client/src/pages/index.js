3b8949127ba43af829e81d836f0560ca99978ae2
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-08 14:30:04 +0700

    Done #5

--- start/client/src/pages/index.js
+++ start/client/src/pages/index.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from 'react';
-import { Router } from '@reach/router';
+import React, { Fragment } from "react";
+import { Router } from "@reach/router";
 
-import Launch from './launch';
-import Launches from './launches';
-import Cart from './cart';
-import Profile from './profile';
-import { Footer, PageContainer } from '../components';
+import Launch from "./launch";
+import Launches from "./launches";
+import Cart from "./cart";
+import Profile from "./profile";
+import { Footer, PageContainer } from "../components";
 
 export default function Pages() {
   return (
     <Fragment>
-      <PageContainer>
+      {/* <PageContainer>
         <Router primary={false} component={Fragment}>
           <Launches path="/" />
           <Launch path="launch/:launchId" />
           <Cart path="cart" />
           <Profile path="profile" />
         </Router>
-      </PageContainer>
+      </PageContainer> */}
       <Footer />
     </Fragment>
   );
