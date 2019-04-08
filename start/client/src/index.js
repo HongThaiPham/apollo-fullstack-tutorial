efb1458641ab70664fe5353e35154558e948647a
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-08 14:52:29 +0700

    Done #7

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -9,11 +9,23 @@ import Pages from "./pages";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:4000/"
+  uri: "http://localhost:4000/",
+  headers: {
+    authorization: localStorage.getItem("token")
+  }
 });
 const client = new ApolloClient({
   cache,
-  link
+  link,
+  resolvers,
+  typeDefs
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: []
+  }
 });
 
 ReactDOM.render(
