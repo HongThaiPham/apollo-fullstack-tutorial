14417af82a5309990ab59b129b480dc53fa65213
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-08 09:56:51 +0700

    Done #2

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,19 @@
 const { ApolloServer } = require("apollo-server");
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userApi: new UserAPI({ store })
+  })
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
